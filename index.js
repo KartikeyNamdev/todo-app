725ef5f1d9606ec68fbe4dacdcb0c3c6f5f76972
KartikeyNamdev <kartikeynamdev2003@gmail.com>
2024-08-15 13:50:18 +0530

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const { createToDo } = require("./types");
 const app = express();
 
 app.use(express.json());
 
-app.post("/todos", function (req, res) {});
+app.post("/todo", function (req, res) {
+  const createPayLoad = req.body;
+  const safeParsed = createToDo.safeParse(createPayLoad);
+  if (!safeParsed.success) {
+    res.status(411).json({
+      msg: "You have send wrong input",
+    });
+    return;
+  }
+  // put it in mongoDB
+});
 
-app.get("/todos", function (req, res) {});
+app.get("/todo", function (req, res) {});
 
-app.put("/completed", function (req, res) {});
+app.put("/completed", function (req, res) {
+  const updateToDo = req.body;
+  const safeParsed = updateToDo.safeParse(updateToDo);
+  if (!safeParsed.success) {
+    res.status(411).json({
+      msg: "You have send wrong input",
+    });
+    return;
+  }
+  //update todo in mongoDB
+});
